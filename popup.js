a42605e7aaf2cec33b4023ecdadaf7804c0bdeb8
kikicarlson <157525963+kikicarlson@users.noreply.github.com>
2024-02-11 21:16:17 +0100

    test

--- popup.js
+++ popup.js
@@ -1,18 +1,23 @@
 document.getElementById('startFollow').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: clickFollowButtons,
-      });
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: clickFollowButtons,
+        });
     });
-  });
+});
 
-  async function clickFollowButtons() {
-    const followButtons = document.querySelectorAll('div[data-testid="UserCell"] div[role="button"][aria-label*="Follow"]');
-    
-    for (let elem of followButtons.slice(0,10)){
-        if (elem.innerText== "Follow"){
-            await setTimeout(() => {elem.click();}, Math.random() * 10000 + 2000); // Sleep between 2s and 7s
+function clickFollowButtons() {
+    const followButtons = [...document.querySelectorAll('div[data-testid="UserCell"] div[role="button"][aria-label*="Follow"]')];
+
+    async function clickButtonWithDelay(button) {
+        if (button.innerText === "Follow") {
+            // Schedule the click action after a delay
+            setTimeout(() => button.click(), Math.random() * 30000 + 2000); // Sleep between 2s and 12s
         }
-    } 
+    }
+
+    followButtons.slice(0, 3).forEach(elem => {
+        clickButtonWithDelay(elem);
+    });
 }
