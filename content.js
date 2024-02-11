4c275c3bdb288d12e6ca729c9fc84f2df4118856
kikicarlson <157525963+kikicarlson@users.noreply.github.com>
2024-02-11 21:06:45 +0100

    test

--- content.js
+++ content.js
@@ -1,17 +1,16 @@
-function clickFollowButtons() {
+async function clickFollowButtons() {
     const followButtons = document.querySelectorAll('div[data-testid="UserCell"] div[role="button"][aria-label*="Follow"]');
-  
-    function clickButton(index) {
-      if (index >= followButtons.length) return;
-      followButtons[index].click();
-      console.log(`Clicked follow button ${index + 1} of ${followButtons.length}`);
-  
-      // Random sleep between clicks
-      setTimeout(() => clickButton(index + 1), Math.random() * 5000 + 2000); // Sleep between 2s and 7s
-    }
-  
-    clickButton(0);
-  }
+    let newFollowButtons= []
+    for (let elem of followButtons){
+        if (elem.innerText== "Follow"){
+            elem.click();
+            await setTimeout(() => {}, Math.random() * 200 + 2000); // Sleep between 2s and 7s
+        }
+    } 
+}
+
+
+
   
   // Execute when content script loads to auto-start the process or based on some other trigger
   clickFollowButtons();
--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
 document.getElementById('startFollow').addEventListener('click', () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        // Send a message to the active tab
-        chrome.tabs.sendMessage(tabs[0].id, {action: "followButtonClick"});
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: clickFollowButtons,
+      });
     });
-});
+  });
+
+  async function clickFollowButtons() {
+    const followButtons = document.querySelectorAll('div[data-testid="UserCell"] div[role="button"][aria-label*="Follow"]');
+    
+    for (let elem of followButtons.slice(0,10)){
+        if (elem.innerText== "Follow"){
+            await setTimeout(() => {elem.click();}, Math.random() * 10000 + 2000); // Sleep between 2s and 7s
+        }
+    } 
+}
